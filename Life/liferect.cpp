cf870ed4069787d2c4b6bd30dbd9ca7d75f680de
Rocker98 <y.petrunyak@gmail.com>
2016-10-21 18:51:20 +0300

    2 commit(add lifeRect class)

--- Life/liferect.cpp
+++ Life/liferect.cpp
@@ -1,6 +1,23 @@
 #include "liferect.h"
 
-LifeRect::LifeRect()
+LifeRect::LifeRect(qreal x, qreal y, qreal w, qreal h, QGraphicsItem* parent):QGraphicsRectItem(x,y,w,h,parent)
 {
+    this->setBrush(QBrush(Qt::white));
+}
 
+void LifeRect::change_color()
+{
+    if(this->brush().color()==Qt::white)
+    {
+        this->setBrush(QBrush(Qt::black));
+    }
+    else
+    {
+        this->setBrush(QBrush(Qt::white));
+    }
+}
+
+void LifeRect::mousePressEvent()
+{
+    change_color();
 }
